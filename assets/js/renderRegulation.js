823806f6c026ca643ea3394a5ba44bfc5eeeaebc
rainxf <rainxf.32@gmail.com>
2026-06-15 04:12:12 +0900

    Add GM note to regulation reward labels

--- assets/js/renderRegulation.js
+++ assets/js/renderRegulation.js
@@ -1,6 +1,6 @@
 import { loadJson } from "./dataLoader.js";
 
-const REGULATION_DATA_PATH = "data/regulation.json?v=20260614-angel-route-rules";
+const REGULATION_DATA_PATH = "data/regulation.json?v=20260615-gm-reward-note";
 
 const TOC_ITEMS = [
   { id: "schedule", label: "開催スケジュール" },
